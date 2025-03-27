21f87a4468fc15d91077c554898ebeb77a887d5f
Vitor Barroso <vitorlauvresbarroso@gmail.com>
2025-03-27 19:55:38 -0300

    .

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -68,7 +68,7 @@ class CheckoutController extends Controller
     public function verify_pay($payment_id)
     {
         $getPayment = CelcashPayments::where('galax_pay_id', $payment_id)
-            ->where('status', g'payed_pix')
+            ->where('status', 'payed_pix')
             ->exists();
 
         $returnData = $getPayment ? true : false;
