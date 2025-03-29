825e13c09373e5ca5bbe21487b23e530a334efb0
leandrocubas-at-975050087534 <leandro.cubas@flamepay.com.br>
2025-03-28 23:54:09 -0300

    passando os daados do pixel para formatacão

--- app/Http/Controllers/CelCashController.php
+++ app/Http/Controllers/CelCashController.php
@@ -370,7 +370,7 @@ class CelCashController extends Controller
                         ]); // salvo na tabela o evento do pixel para disparar após a confirmacao do pagamento
                     }
                 }else{
-                    $pixel_data=PixelEventService::FormatDataPixel($request->pixel_data);
+                    $pixel_data=PixelEventService::FormatDataPixel($validatedData['pixel_data']);
                     Log::info("Colocando na fila o evento para disparar o pixel", ["pixel" => $pixel_data]);
                     event(new PixelEvent($getPrincipalOffer->id, 'Purchase', $pixel_data, $request->header('x-transaction-id')));
                 }
